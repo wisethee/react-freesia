d666412b68cc87ddd62846e98ee95b1d2142ca6c
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-10-31 20:14:08 +0000

    fix: useFrame types

--- apps/freesia/src/app/components/custom-object/custom-object.component.tsx
+++ apps/freesia/src/app/components/custom-object/custom-object.component.tsx
@@ -1,5 +1,26 @@
+import * as THREE from 'three';
+
 const CustomObject = () => {
-  return <div>CustomObject</div>;
+  const verticesCount = 10 * 3;
+  const positions = new Float32Array(verticesCount * 3);
+
+  for (let i = 0; i < verticesCount * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 3;
+  }
+
+  return (
+    <mesh>
+      <bufferGeometry>
+        <bufferAttribute
+          attach="attributes-position"
+          count={verticesCount}
+          itemSize={3}
+          array={positions}
+        />
+      </bufferGeometry>
+      <meshBasicMaterial color="red" side={THREE.DoubleSide} />
+    </mesh>
+  );
 };
 
 export default CustomObject;
