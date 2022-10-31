fb6100a6fc22a243898b3c9f30c88b68488d2763
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-10-31 19:14:46 +0000

    feat: add orbit controls

--- apps/freesia/src/app/components/experience/experience.component.tsx
+++ apps/freesia/src/app/components/experience/experience.component.tsx
@@ -1,5 +1,6 @@
 import { Fragment, useRef } from 'react';
 import { ThreeElements, useFrame } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
 const Experience = () => {
   const cubeRef = useRef<ThreeElements['mesh']>(null);
@@ -11,19 +12,23 @@ const Experience = () => {
 
   return (
     <Fragment>
+      <OrbitControls />
+      <directionalLight position={[1, 2, 3]} intesity={1.5} />
+      <ambientLight intesity={0.3} />
+
       <mesh ref={cubeRef} position={[2, 0, 0]}>
         <boxGeometry />
-        <meshBasicMaterial color="mediumpurple" />
+        <meshStandardMaterial color="mediumpurple" />
       </mesh>
 
       <mesh position={[-2, 0, 0]}>
         <sphereGeometry />
-        <meshBasicMaterial color="orange" />
+        <meshStandardMaterial color="orange" />
       </mesh>
 
       <mesh scale={9} rotation={[-Math.PI * 0.5, 0, 0]} position={[0, -1, 0]}>
         <planeGeometry />
-        <meshBasicMaterial color="greenyellow" />
+        <meshStandardMaterial color="greenyellow" />
       </mesh>
     </Fragment>
   );
