048102f8c365b0998fd31b3cb47a79b0d0572da3
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-11-01 10:39:22 +0000

    feat: add ed text

--- apps/freesia/src/app/components/experience/experience.component.tsx
+++ apps/freesia/src/app/components/experience/experience.component.tsx
@@ -1,9 +1,17 @@
 import { Fragment, useRef } from 'react';
-import { OrbitControls, PivotControls } from '@react-three/drei';
+import {
+  OrbitControls,
+  PivotControls,
+  Html,
+  Text,
+  Float,
+  MeshReflectorMaterial,
+} from '@react-three/drei';
 import { Mesh } from 'three';
 
 const Experience = () => {
   const cubeRef = useRef<Mesh | null>(null);
+  const sphereRef = useRef<Mesh | null>(null);
 
   return (
     <Fragment>
@@ -29,16 +37,46 @@ const Experience = () => {
         lineWidth={3}
         scale={0.5}
       >
-        <mesh position={[-2, 0, 0]}>
+        <mesh ref={sphereRef} position={[-2, 0, 0]}>
           <sphereGeometry />
           <meshStandardMaterial color="orange" />
+          <Html
+            position={[1, 1, 0]}
+            wrapperClass="text"
+            center
+            distanceFactor={3}
+            occlude={[sphereRef, cubeRef]}
+          >
+            This is a sphere
+          </Html>
         </mesh>
       </PivotControls>
 
       <mesh scale={9} rotation={[-Math.PI * 0.5, 0, 0]} position={[0, -1, 0]}>
         <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
+        {/* <meshStandardMaterial color="greenyellow" /> */}
+        <MeshReflectorMaterial
+          mirror={0.3}
+          mixBlur={1}
+          mixStrength={0.3}
+          resolution={1024}
+          color="greenyellow"
+          blur={[0, 0]}
+        />
       </mesh>
+
+      <Float>
+        <Text
+          font="../../../assets/bangers.woff"
+          fontSize={1}
+          color="salmon"
+          position={[0, 1, -2]}
+          maxWidth={2}
+          textAlign="center"
+        >
+          R3F Text
+        </Text>
+      </Float>
     </Fragment>
   );
 };
