241722fd9a0ba6b025903ab632cb1adeeae05d45
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-11-02 10:05:58 +0000

    feat: add directional light helper

--- apps/freesia/src/app/components/experience/experience.component.tsx
+++ apps/freesia/src/app/components/experience/experience.component.tsx
@@ -1,18 +1,34 @@
 import { Fragment, useRef } from 'react';
-import { OrbitControls } from '@react-three/drei';
-import { Mesh } from 'three';
+import { OrbitControls, useHelper } from '@react-three/drei';
+import { DirectionalLight, Mesh } from 'three';
 import { Perf } from 'r3f-perf';
+import { useFrame } from '@react-three/fiber';
+import * as THREE from 'three';
 
 const Experience = () => {
   const cubeRef = useRef<Mesh | null>(null);
   const sphereRef = useRef<Mesh | null>(null);
 
+  const directionalLightRef = useRef<DirectionalLight | null>(null);
+
+  useHelper(directionalLightRef, THREE.DirectionalLightHelper, 0.5);
+
+  useFrame((state, delta) => {
+    if (!cubeRef.current) throw Error('cubeRef not assigned');
+    cubeRef.current.rotation.y += delta * 0.2;
+  });
+
   return (
     <Fragment>
       <Perf position="bottom-right" />
       <OrbitControls makeDefault />
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <directionalLight
+        ref={directionalLightRef}
+        position={[1, 2, 3]}
+        intensity={1.5}
+      />
       <ambientLight intensity={0.3} />
+      <color args={['ivory']} attach="background" />
 
       <mesh position-x={-2}>
         <sphereGeometry />
