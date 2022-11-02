c56fb1cc4dde19a28e9d783fde52b86a738d22e8
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-11-02 09:12:27 +0000

    add r3f-perf

--- apps/freesia/src/app/app.tsx
+++ apps/freesia/src/app/app.tsx
@@ -1,18 +1,23 @@
 import { Canvas } from '@react-three/fiber';
+import { Leva } from 'leva';
+import { Fragment } from 'react';
 import Experience from './components/experience/experience.component';
 
 const App = () => {
   return (
-    <Canvas
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [3, 2, 6],
-      }}
-    >
-      <Experience />
-    </Canvas>
+    <Fragment>
+      <Leva collapsed />
+      <Canvas
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [3, 2, 6],
+        }}
+      >
+        <Experience />
+      </Canvas>
+    </Fragment>
   );
 };
 
